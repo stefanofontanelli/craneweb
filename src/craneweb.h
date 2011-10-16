e42c8258f0cf0c5666bcd0ae804c5a327c2d9628
Francesco Romani <fromani@gmail.com>
2011-10-16 10:22:50 +0200

    docs round one

--- src/craneweb.h
+++ src/craneweb.h
@@ -34,6 +34,7 @@ enum {
                                        is silently dropped.
                                    */
 };
+// TODO: doxygen anchor
 
 /*** logger **************************************************************/
 
@@ -106,25 +107,106 @@ typedef struct crwinstance_ CRW_Instance;
 /** \var typedef CRW_Request
     \brief A CRW_Request abstracts an HTTP request to your application.
 
+    Accessing the fields a CRW_Request allow the application to understand
+    what to do and how properly respond to the request itself.
+
+    Both high-level and low-level (raw HTTP fileds) accessors are provided.
+
+    The CRW_Request are provided, already filled and set up, by the 
+    craneweb runtime to your application. There is (should) no need
+    to build or to free or to modify them on the application.
+    Thereof, all the accessor function are getters only.
 */ 
 typedef struct crwrequest_ CRW_Request;
 
+/** \enum CRW_RequestMethod
+    \brief the HTTP method of the request.
+*/
 typedef enum crwrequestmethod_ {
-    CRW_REQUEST_METHOD_UNSUPPORTED = -1,
-    CRW_REQUEST_METHOD_UNKNOWN = 0,
-    CRW_REQUEST_METHOD_GET,
-    CRW_REQUEST_METHOD_HEAD,
-    CRW_REQUEST_METHOD_POST,
-    CRW_REQUEST_METHOD_PUT,
-    CRW_REQUEST_METHOD_DELETE
+    CRW_REQUEST_METHOD_UNSUPPORTED = -1, /**< invalid data marker */
+    CRW_REQUEST_METHOD_UNKNOWN = 0,      /**< on error (runtime) */
+    CRW_REQUEST_METHOD_GET,              /**< GET */
+    CRW_REQUEST_METHOD_HEAD,             /**< HEAD */
+    CRW_REQUEST_METHOD_POST,             /**< POST */
+    CRW_REQUEST_METHOD_PUT,              /**< PUT */
+    CRW_REQUEST_METHOD_DELETE            /**< DELETE */
 } CRW_RequestMethod;
 
+/** \fn CRW_request_get_method
+    \brief get the HTTP method of a given request
+
+    \param req CRW_Request to be examined.
+    \return the CRW_RequestMethod of the given request.
+
+    \see CRW_RequestMethod
+*/
 CRW_RequestMethod CRW_request_get_method(const CRW_Request *req);
 
+/** \fn CRW_request_count_headers
+    \brief tell how many attached HTTP headers are avalaible for
+           the given request.
+
+    In the common case, this function returns the number of H
+    headers attached to the HTTP request wrapped to craneweb.
+    So, the real number of headers passed on the wire.
+
+    However, given the implementation limits of craneweb, on
+    certain (rare) cases, only the first N can be provided.
+    the remainder of the headers are silently discarded.
+
+    \param req CRW_Request to be examined.
+    \return <0 on error, the number of avalaible headers on success.
+*/
 int CRW_request_count_headers(const CRW_Request *req);
+
+/** \fn CRW_request_get_header_by_idx
+    \brief access the header data.
+
+    Access the HTTP header data (in a readonly way) using an index.
+    The HTTP headers are indexed from 0 (zero) to N, [0,N) where
+    N is the upper bound is provided by CRW_request_count_headers.
+
+    The header data is returned preserving the case.
+
+    \param req CRW_Request to be examined.
+    \param idx index of the header data requested.
+    \param[out] header const pointer to be set to the idx-th header key.
+    \param[out] value const pointer to be set to the idx-th header value.
+    \return 0 on success, <0 on error.
+
+    \see CRW_request_count_headers
+*/
 int CRW_request_get_header_by_idx(const CRW_Request *req, int idx,
                                   const char **header, const char **value);
+
+/** \fn CRW_request_get_header_value
+    \brief access the header value given its name.
+
+    Access the HTTP header data (in a readonly way) using the header name.
+    The name search is done in a case INsenstive way.
+    The header data is returned preserving the case.
+
+    \param req CRW_Request to be examined.
+    \param header the header name (key) to be searched.
+    \return NULL on error, the value of the header key on success.
+
+    \see CRW_request_get_header_by_idx
+    \see CRW_request_count_headers
+*/
 const char *CRW_request_get_header_value(const CRW_Request *req, const char *header);
+
+/** \fn CRW_request_is_xhr
+    \brief checks if the request is made by the client using
+           XmlHttpRequest.
+
+    CAUTION: proper detection required that the client code honores the
+    custom http header. This call can give wrong result if the client
+    is non-compliant.
+
+    \param req CRW_Request to be examined.
+    \return !0 if the request is detected made using XmlHttpRequest,
+            0 otherwise.
+*/
 int CRW_request_is_xhr(const CRW_Request *req);
 
 /*** response ************************************************************/
@@ -177,6 +259,13 @@ int CRW_instance_add_handler(CRW_Instance *inst, CRW_Handler *handler);
 
 /*** runtime(!) **********************************************************/
 
+/** \struct CRW_Config
+    \brief runtime configuration of a CRW_Instance.
+
+    Export the tunables of the CRW_Instance.
+
+    Client code needs to properly fill it for running a CRW_Instance.
+*/
 typedef struct crwconfig_ CRW_Config;
 struct crwconfig_ {
     const char *host;           /**< host IP to listen on */
@@ -184,6 +273,16 @@ struct crwconfig_ {
     const char *document_root;  /**< document root (static files)*/
 };
 
+/**< \fn CRW_run
+     \brief runs a CRW_Instance, allowing it to serve requests.
+
+     \param instance the CRW_Instance to be run.
+     \param cfg CRW_Config describing how to run the given instance.
+     \return 0 on success, <0 on error.
+
+     \see CRW_Instance
+     \see CRW_Config
+*/
 int CRW_run(CRW_Instance *instance, const CRW_Config *cfg);
 
 /*** that's all folks! ***************************************************/
